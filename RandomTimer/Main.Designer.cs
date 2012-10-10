66d93dc779db5c6bc0a96bf6a7ccbdb95aa7f620
Arthur Vanderbilt <arthurvanderbilt@gmail.com>
2012-10-10 12:11:58 -0500

    Initial commit.

---
+++ RandomTimer/Main.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace RandomTimer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.NotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.StartContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.StopContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.StatusStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.DurationNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.contextMenuStrip.SuspendLayout();
+            this.StatusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DurationNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 40);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 29);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "&Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Enabled = false;
+            this.StopButton.Location = new System.Drawing.Point(93, 40);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 29);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "S&top";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // NotifyIcon
+            // 
+            this.NotifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.NotifyIcon.Text = "NotifyIcon";
+            this.NotifyIcon.Visible = true;
+            this.NotifyIcon.BalloonTipClicked += new System.EventHandler(this.NotifyIcon_BalloonTipClicked);
+            this.NotifyIcon.DoubleClick += new System.EventHandler(this.NotifyIcon_DoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StartContextMenuItem,
+            this.StopContextMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(153, 74);
+            // 
+            // StartContextMenuItem
+            // 
+            this.StartContextMenuItem.Name = "StartContextMenuItem";
+            this.StartContextMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.StartContextMenuItem.Text = "Start";
+            this.StartContextMenuItem.Click += new System.EventHandler(this.StartContextMenuItem_Click);
+            // 
+            // StopContextMenuItem
+            // 
+            this.StopContextMenuItem.Enabled = false;
+            this.StopContextMenuItem.Name = "StopContextMenuItem";
+            this.StopContextMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.StopContextMenuItem.Text = "Stop";
+            this.StopContextMenuItem.Click += new System.EventHandler(this.StopContextMenuItem_Click);
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusStripLabel});
+            this.StatusStrip.Location = new System.Drawing.Point(0, 82);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(181, 25);
+            this.StatusStrip.TabIndex = 3;
+            // 
+            // StatusStripLabel
+            // 
+            this.StatusStripLabel.Name = "StatusStripLabel";
+            this.StatusStripLabel.Size = new System.Drawing.Size(69, 20);
+            this.StatusStripLabel.Text = "Stopped.";
+            // 
+            // DurationNumericUpDown
+            // 
+            this.DurationNumericUpDown.Location = new System.Drawing.Point(12, 12);
+            this.DurationNumericUpDown.Name = "DurationNumericUpDown";
+            this.DurationNumericUpDown.Size = new System.Drawing.Size(156, 22);
+            this.DurationNumericUpDown.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(181, 107);
+            this.Controls.Add(this.DurationNumericUpDown);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Main";
+            this.Text = "Random Timer";
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DurationNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.NotifyIcon NotifyIcon;
+        private System.Windows.Forms.StatusStrip StatusStrip;
+        private System.Windows.Forms.NumericUpDown DurationNumericUpDown;
+        private System.Windows.Forms.ToolStripStatusLabel StatusStripLabel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem StartContextMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem StopContextMenuItem;
+    }
+}
---
+++ RandomTimer/Main.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+using RandomTimer.Properties;
+
+namespace RandomTimer
+{
+    public partial class Main : Form
+    {
+        private readonly Random _random;
+        private readonly Timer Timer;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            _random = new Random();
+
+            Timer = new Timer();
+            Timer.Tick += new EventHandler(Timer_Tick);
+
+            Icon = Resources.Clock;
+
+            NotifyIcon.Icon = Resources.Clock;
+            NotifyIcon.BalloonTipText = "The random timer has elapsed.";
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            if (DurationNumericUpDown.Value == 0)
+                return;
+
+            var randomInterval = _random.Next((int)DurationNumericUpDown.Value * 1000 * 60);
+
+            Timer.Interval = randomInterval;
+            Timer.Start();
+            StatusStripLabel.Text = "Running ...";
+            NotifyIcon.Text = "Running ...";
+
+            StartButton.Enabled = false;
+            StopButton.Enabled = true;
+            StartContextMenuItem.Enabled = false;
+            StopContextMenuItem.Enabled = true;
+            DurationNumericUpDown.Enabled = false;
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+
+        private void StopTimer()
+        {
+            Timer.Stop();
+            StatusStripLabel.Text = "Stopped.";
+            NotifyIcon.Text = "Stopped.";
+
+            StartButton.Enabled = true;
+            StopButton.Enabled = false;
+            StartContextMenuItem.Enabled = true;
+            StopContextMenuItem.Enabled = false;
+            DurationNumericUpDown.Enabled = true;
+        }
+
+        private void Timer_Tick(object theObject, EventArgs eventArgs)
+        {
+            Timer.Stop();
+            StatusStripLabel.Text = "Stopped.";
+            NotifyIcon.Text = "Stopped.";
+
+            // WindowState = FormWindowState.Normal;
+
+            NotifyIcon.ShowBalloonTip(30*1000);
+
+            StartButton.Enabled = true;
+            StopButton.Enabled = false;
+            StartContextMenuItem.Enabled = true;
+            StopContextMenuItem.Enabled = false;
+            DurationNumericUpDown.Enabled = true;
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            switch (WindowState)
+            {
+                case FormWindowState.Minimized:
+                    NotifyIcon.Visible = true;
+                    ShowInTaskbar = false;
+                    break;
+                case FormWindowState.Normal:
+                    NotifyIcon.Visible = false;
+                    ShowInTaskbar = true;
+                    BringToFront();
+                    break;
+            }
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void NotifyIcon_BalloonTipClicked(object sender, EventArgs e)
+        {
+            StartTimer();
+        }
+
+        private void StartContextMenuItem_Click(object sender, EventArgs e)
+        {
+            StartTimer();
+        }
+
+        private void StopContextMenuItem_Click(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+    }
+}
